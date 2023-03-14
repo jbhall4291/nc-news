5f25d7b40eb301ea04bffad017790f7ded84adf1
johnny <jbhall4291@gmail.com>
2023-03-14 15:43:58 +0000

    add basic functionality to navbar in order to get back to homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import "../styling/Navbar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styling/Navbar.css";
 
 const Navbar = () => {
-    return (
-        <section className="navbar">
-            <h2 className="navbar__h2">this is the navbar placeholder</h2>
-        </section>
-    );
+  return (
+    <section className="navbar">
+      <Link to="/">
+        <h2 className="navbar__h2">View All Articles</h2>
+      </Link>
+      <Link to="/placeholder">
+        <h2 className="navbar__h2">Placeholder For Topic 1</h2>
+      </Link>
+      <Link to="/placeholder">
+        <h2 className="navbar__h2">Placeholder For Topic 2</h2>
+      </Link>
+    </section>
+  );
 };
 
 export default Navbar;
