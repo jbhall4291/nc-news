5541c1fdd2573e45f5b1679a106a8a0de060ee9a
johnny <jbhall4291@gmail.com>
2023-03-18 23:36:02 +0000

    style navbar items on hover or when active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/Navbar.css";
 
 const Navbar = () => {
+  const [selectedTopic, setSelectedTopic] = useState("allTopics");
+
+  function handleTopicClick(topic) {
+    setSelectedTopic(topic);
+  }
+
   return (
     <section className="navbar">
-      <Link className="Navbar__Link" to="/">
+      <Link
+        to="/"
+        onClick={() => handleTopicClick("allTopics")}
+        className={`Navbar__Link ${
+          selectedTopic === "allTopics"
+            ? "Navbar__Link--active"
+            : "Navbar__Link"
+        }`}
+      >
         <b>All Topics</b>
       </Link>
-      <Link className="Navbar__Link" to="/topics/coding">
+
+      <Link
+        to="/topics/coding"
+        onClick={() => handleTopicClick("coding")}
+        className={`Navbar__Link ${
+          selectedTopic === "coding" ? "Navbar__Link--active" : "Navbar__Link"
+        }`}
+      >
         Coding
       </Link>
-      <Link className="Navbar__Link" to="/topics/cooking">
+      <Link
+        to="/topics/cooking"
+        onClick={() => handleTopicClick("cooking")}
+        className={`Navbar__Link ${
+          selectedTopic === "cooking" ? "Navbar__Link--active" : "Navbar__Link"
+        }`}
+      >
         Cooking
       </Link>
-      <Link className="Navbar__Link" to="/topics/football">
+      <Link
+        to="/topics/football"
+        onClick={() => handleTopicClick("football")}
+        className={`Navbar__Link ${
+          selectedTopic === "football" ? "Navbar__Link--active" : "Navbar__Link"
+        }`}
+      >
         Football
       </Link>
     </section>
