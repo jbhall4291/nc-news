31c69534b64dcfd68a9a1e27101bdc2b701b10d2
johnny <jbhall4291@gmail.com>
2023-05-08 21:37:46 +0100

    incorporate navbar into head, make sticky

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Articles from "./components/Articles";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+
 import SingleArticle from "./components/SingleArticle";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Articles />} />
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+
 import "../styling/Header.css";
+import Navbar from "./Navbar";
 
 const Header = () => {
-  return <h1 className="header__h1">NewsBuzz</h1>;
+  return (
+    <header>
+      <h1 className="header__h1">NewsBuzz</h1>
+      <Navbar />
+    </header>
+  );
 };
 
 export default Header;
